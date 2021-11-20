38dba74372c354aaffc11e0b9aabae1269eec85d
Ben S <benslinde@gmail.com>
2021-11-20 08:00:27 -0500

    fixed error is server.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-//const routes = require('./routes');
 
-// require my routes with graphQL
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-//require apollo server
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-// apply apollo server to express as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -35,20 +31,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
-
-
-
-
-
-
-// not needed for apollo
-//app.use(routes);
-
-// need the graphQL path
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
